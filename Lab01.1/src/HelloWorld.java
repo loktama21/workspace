d444d02ac19ced6f678a876a27e7b44434449006
Lok Tamang <84638841+loktama21@users.noreply.github.com>
2022-11-04 16:54:38 -0500

    Update HelloWorld.java
    changes made by sang.

--- Lab01.1/src/HelloWorld.java
+++ Lab01.1/src/HelloWorld.java
@@ -2,8 +2,8 @@ class HelloWorld {
     //I am making changes from Intellij
     //Lok from intellij
     public static void main(String[] args) {
-        //This is the change I made.
-        //Lok Tamang
+        //This is the change I made second time.
+        //By Sang
         System.out.println("Hello from Intellij");
     }
 }
